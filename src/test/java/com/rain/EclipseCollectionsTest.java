ad88aaf79013a3452a61da5a832aef5ad67dd1be
xueyu <xueyu9641@gmail.com>
2024-09-08 00:18:47 +0800

    add: Eclipse Collections 集合框架测试

---
+++ src/test/java/com/rain/EclipseCollectionsTest.java
@@ -0,0 +1,20 @@
+package com.rain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class EclipseCollectionsTest {
+
+    @Test
+    void test() {
+        // 创建一个可变列表
+        MutableList<String> list = Lists.mutable.of("Apple", "Banana", "Orange");
+        list.add("aaaaaaaaaaaaaa");
+        // 选择长度大于 5 的元素
+        MutableList<String> filtered = list.select(fruit -> fruit.length() > 5);
+        System.out.println("Filtered List: " + filtered);
+    }
+}
