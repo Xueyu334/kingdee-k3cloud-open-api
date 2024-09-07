d4b6dee9bfbefb225b3254c5fefbc53c3fbc282e
xueyu <xueyu9641@gmail.com>
2024-09-07 21:35:38 +0800

    add: Caffeine 高速缓存框架 测试

---
+++ src/test/java/com/rain/CaffeineTest.java
@@ -0,0 +1,26 @@
+package com.rain;
+
+import cn.hutool.core.thread.ThreadUtil;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class CaffeineTest {
+
+
+    @Test
+    void test() {
+        Cache<Object, Object> cache = Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(3, TimeUnit.SECONDS)
+                .build();
+        cache.put("as1", "as");
+        ThreadUtil.safeSleep(1000);
+        Object as1 = cache.get("as1", k -> null);
+        log.info(as1.toString());
+    }
+}
