6561faa750a33497e0f58a2fa490107ec61f0090
xueyu <xueyuxe@163.com>
2026-01-14 13:04:53 +0800

    chore: 更新依赖版本并移除 Hutool 线程工具 - 将 Spring Boot 版本从 3.3.4 升级至 3.5.9 - 将 Apache Commons Lang3 版本从 3.15.0 升级至 3.20.0 - 移除 Hutool 的 ThreadUtil 导入和使用 - 将测试中的 ThreadUtil.safeSleep 替换为 Thread.sleep

--- src/test/java/com/rain/caffeine/CaffeineTest.java
+++ src/test/java/com/rain/caffeine/CaffeineTest.java
@@ -1,6 +1,6 @@
 package com.rain.caffeine;
 
-import cn.hutool.core.thread.ThreadUtil;
+
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +13,13 @@ public class CaffeineTest {
 
 
     @Test
-    void test() {
+    void test() throws InterruptedException {
         Cache<Object, Object> cache = Caffeine.newBuilder()
                 .maximumSize(100)
                 .expireAfterWrite(3, TimeUnit.SECONDS)
                 .build();
         cache.put("as1", "as");
-        ThreadUtil.safeSleep(1000);
+        Thread.sleep(1000);
         Object as1 = cache.get("as1", k -> null);
         log.info(as1.toString());
     }
